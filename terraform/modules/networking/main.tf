0d0aefd1a8c009cfa755befd9b5b49ca280127fd
ZeeshanAslam1984 <zesh1984@gmail.com>
2026-07-08 20:20:38 +0500

    feat(terraform): add public and private subnets

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -5,3 +5,30 @@ resource "aws_internet_gateway" "main" {
     Name = "${var.environment}-igw"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id = var.vpc_id
+
+  cidr_block = var.public_subnet_cidr
+
+  availability_zone = var.availability_zone
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.environment}-public-subnet"
+  }
+}
+
+
+resource "aws_subnet" "private" {
+  vpc_id = var.vpc_id
+
+  cidr_block = var.private_subnet_cidr
+
+  availability_zone = var.availability_zone
+
+  tags = {
+    Name = "${var.environment}-private-subnet"
+  }
+}
--- terraform/modules/networking/variables.tf
+++ terraform/modules/networking/variables.tf
@@ -7,3 +7,20 @@ variable "environment" {
   description = "Environment name"
   type        = string
 }
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for public subnet"
+  type        = string
+}
+
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for private subnet"
+  type        = string
+}
+
+
+variable "availability_zone" {
+  description = "Availability Zone"
+  type        = string
+}
