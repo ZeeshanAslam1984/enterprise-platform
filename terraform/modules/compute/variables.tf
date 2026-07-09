1a8db0d75e13cb17fee4c0ed3f2be54d83df0f11
ZeeshanAslam1984 <zesh1984@gmail.com>
2026-07-09 18:05:57 +0500

    feat(terraform): add compute module files

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,35 @@
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+  type        = string
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the instance will be launched"
+  type        = string
+}
+
+variable "security_group_ids" {
+  description = "List of security group IDs"
+  type        = list(string)
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the EC2 instance"
+  type        = map(string)
+  default     = {}
+}
