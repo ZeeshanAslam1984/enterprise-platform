ae81753286a2831021e56fd48cd0292d8891800e
ZeeshanAslam1984 <zesh1984@gmail.com>
2026-07-09 14:10:52 +0500

    feat(terraform): complete networking module

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -53,3 +53,41 @@ resource "aws_route_table_association" "public" {
 
   route_table_id = aws_route_table.public.id
 }
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.environment}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "${var.environment}-nat-gateway"
+  }
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.environment}-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
